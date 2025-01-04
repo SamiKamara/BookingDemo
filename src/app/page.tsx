f1cdad3e0de1b8aad8da1dc1bdf4dce3db83ca3e
Sami Nikkanen <sami.nikkanen@tuni.fi>
2025-01-04 12:19:58 +0200

    feat: add border and rounded corners to iframe widget

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -38,7 +38,10 @@ export default function Home() {
             src="https://booking.setmore.com/scheduleappointment/8283bf7b-0a6b-4ae0-bfa7-46196c8b375f"
             width="100%"
             height="500"
-            style={{ border: "none" }}
+            style={{
+              border: "2px solid #ffc0cb", 
+              borderRadius: "4px",
+            }}
             title="Setmore Booking Widget"
           ></iframe>
         </div>
